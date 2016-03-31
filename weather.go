12356f9ff44227a10e3a44f6ad388fd47dffcc02
Sean Kibler <skibler1223@gmail.com>
2016-03-31 10:36:42 -0400

    removes default for zip require zip input

--- weather.go
+++ weather.go
@@ -10,10 +10,10 @@ import(
   "os"
 )
 
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?zip=%s,%s&appid=%s"
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?zip=%s,%s&APPID=%s"
 
 var weatherAPIToken = flag.String("token", os.Getenv("OPENWEATHERMAP_APPID"), "openweathermap APPID")
-var zipCode = flag.String("zip", "44647", "zip code for weather geography")
+var zipCode = flag.String("zip", "", "zip code for weather geography")
 var countryCode = flag.String("country", "us", "country code (see http://openweathermap.org/API for valid possibilities)")
 var testJson = flag.String("json", "", "read from json file instead of API")
 
@@ -21,21 +21,25 @@ func GetWeather() (fcst string) {
   var body []byte
   var err error
 
+  if *zipCode == "" {
+    log.Fatal("need a zip/postcal code")
+  }
+
   if *testJson != "" {
     body, err = ioutil.ReadFile(*testJson)
 
     if err != nil {
-      log.Panicf("io error: %s\n", err.Error())
+      log.Fatalf("io error: %s\n", err.Error())
     }
   } else {
     if *weatherAPIToken == "" {
-      log.Fatalf("need an API token to use openweathermap API")
+      log.Fatal("need an API token to use openweathermap API")
     }
 
     url := fmt.Sprintf(apiURL, *zipCode, *countryCode, weatherAPIToken)
     resp, err := http.Get(url)
     if err != nil {
-      log.Panicf("received http error %s", err)
+      log.Fatalf("received http error %s", err)
     }
     defer resp.Body.Close()
     body, err = ioutil.ReadAll(resp.Body)
@@ -55,7 +59,7 @@ func GetWeather() (fcst string) {
   weatherData := WeatherData{}
 
   if err := json.Unmarshal(body, &weatherData); err != nil {
-    log.Panicf("json error: %s", err.Error())
+    log.Fatalf("json error: %s", err.Error())
   } else {
     if weatherData.Code != 200 {
       fcst = weatherData.Message
@@ -70,6 +74,5 @@ func GetWeather() (fcst string) {
 // Says hello and prints today's weather forecast
 func main() {
   flag.Parse()
-  fmt.Printf("Hello, %s!\n", *zipCode)
   fmt.Printf("%s\n", GetWeather())
 }
